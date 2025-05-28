c79cf19580acd60682d40fff8ba0c538c482809a
Juliano Quites <juliano.quites@gmail.com>
2025-05-28 00:52:51 -0300

    update

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,17 +1,28 @@
-import LoginPage from "../support/pages/Login.page";
 import CreateCoursePage from "../support/pages/CreateCourse.page";
+import { faker } from "@faker-js/faker";
 
 describe("Fluxo de Criação de Curso", () => {
   beforeEach(() => {
-    cy.fixture("login").then(({ cpf, senha }) => {
-      LoginPage.visit();
-      LoginPage.preencherCredenciais(cpf, senha);
-      LoginPage.submit();
+    cy.fixture("login").then((credenciais) => {
+      cy.login(credenciais.cpf, credenciais.senha);
     });
   });
 
-  it("Deve criar um curso com dados válidos", () => {
+  it("Deve criar um curso com os dados obrigatórios", () => {
+    cy.intercept("POST", "/curso/verificar_campos/").as("verificarCampos");
     CreateCoursePage.visit();
+
+    const cursoMock = {
+      nome: faker.lorem.words(3),
+      descricao: faker.lorem.paragraph(),
+      objetivos: faker.lorem.sentences(2),
+      cargaHoraria: faker.number.int({ min: 1, max: 60 }).toString(),
+    };
+
+    CreateCoursePage.criarCursoBasico(cursoMock);
+    cy.wait(5000);
+
+    cy.wait("@verificarCampos").its("response.statusCode").should("eq", 200);
   });
 
   it("Deve exibir erro ao criar curso sem nome", () => {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+import LoginPage from "../support/pages/Login.page";
+
 Cypress.Commands.add("login", (cpf, senha) => {
   LoginPage.visit();
   LoginPage.preencherCredenciais(cpf, senha);
