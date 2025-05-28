0fdd44cb5f429e7ea97873027e32fb08bae19d31
Juliano Quites <juliano.quites@gmail.com>
2025-05-28 18:17:14 -0300

    upload da segunda parte feita

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -22,13 +22,11 @@ describe("Fluxo de Criação de Curso", () => {
     };
 
     CreateCoursePage.criarCursoBasico(cursoMock);
-
     cy.wait("@verificarCampos").its("response.statusCode").should("eq", 200);
   });
 
   it("Deve bloquear upload de imagem com mais de 2MB", () => {
     CreateCoursePage.visit();
-
     cy.fixture("large-image.jpg", null).then((fileContent) => {
       cy.get("#thumb").attachFile({
         fileContent: fileContent.toString(),
@@ -36,7 +34,6 @@ describe("Fluxo de Criação de Curso", () => {
         mimeType: "image/jpeg",
       });
     });
-
     cy.get("#file-result").should(
       "have.text",
       "Por favor selecione arquivo com menos de 2MB."
@@ -45,38 +42,34 @@ describe("Fluxo de Criação de Curso", () => {
 
   it("Deve recusar a criação de um curso sem o dado obrigatório de descrição", () => {
     CreateCoursePage.visit();
-
     const cursoMock = {
       nome: faker.lorem.words(3),
       objetivos: faker.lorem.sentences(2),
       cargaHoraria: faker.number.int({ min: 1, max: 60 }).toString(),
     };
-
     CreateCoursePage.name.type(cursoMock.nome);
     CreateCoursePage.objetivosAprendizagem.type(cursoMock.objetivos);
     CreateCoursePage.cargaHoraria.type(cursoMock.cargaHoraria);
-
     CreateCoursePage.submitButton.click();
     cy.wait(5000);
-
     CreateCoursePage.submitButton.should("be.visible");
   });
 
-  it("Deve recusar a criação de um curso com o dado obrigatório de nome com menos de dois caracteres", () => {
+  it("Deve recusar a criação de um curso com nome com menos de dois caracteres", () => {
     CreateCoursePage.visit();
-
     const cursoMock = {
       nome: faker.lorem.word(1),
       descricao: faker.lorem.paragraph(),
       objetivos: faker.lorem.sentences(2),
       cargaHoraria: faker.number.int({ min: 1, max: 60 }).toString(),
     };
-
     CreateCoursePage.criarCursoBasico(cursoMock);
     cy.get("#erro-nome").should("be.visible");
   });
-  it("Deve aceitar a criação de um curso com seleção de todos os pre-requisitos", () => {
+
+  it("Deve aceitar a criação de um curso com seleção de todos os pré-requisitos", () => {
     cy.intercept("POST", "/curso/verificar_campos/").as("verificarCampos");
+    CreateCoursePage.visit();
 
     const cursoMock = {
       nome: faker.lorem.words(1),
@@ -85,87 +78,67 @@ describe("Fluxo de Criação de Curso", () => {
       cargaHoraria: faker.number.int({ min: 1, max: 60 }).toString(),
     };
 
-    CreateCoursePage.visit();
-
     CreateCoursePage.name.type(cursoMock.nome);
     CreateCoursePage.descricao.type(cursoMock.descricao);
     CreateCoursePage.objetivosAprendizagem.type(cursoMock.objetivos);
     CreateCoursePage.cargaHoraria.type(cursoMock.cargaHoraria);
     CreateCoursePage.selecionarCursos.check();
-
     CreateCoursePage.submitButton.click();
-    cy.wait(5000);
-
-    CreateCoursePage.submitButton.should("be.visible");
 
     cy.wait("@verificarCampos").its("response.statusCode").should("eq", 200);
   });
+});
 
-  it("Deve editar o nome de um curso com sucesso", () => {
-    let courseNameMock = faker.lorem.words(1);
+describe("Fluxo de Edição de Curso", () => {
+  beforeEach(() => {
+    cy.fixture("login").then((credenciais) => {
+      cy.login(credenciais.cpf, credenciais.senha);
+    });
     EditCoursePage.visit();
     EditCoursePage.visitPageIndexNine();
     cy.get('[data-curso-id="127"]').click();
-
-    CreateCoursePage.name.clear();
-    CreateCoursePage.name.type(courseNameMock);
-    CreateCoursePage.submitButton.click();
-    cy.wait(5000);
-    cy.visit("curso/edit/127");
-    cy.get("#name").should("have.value", courseNameMock);
   });
 
-  it.only("Deve recusar a edição do nome de um curso com menos de dois caracteres", () => {
-    let courseNameMock = faker.lorem.word(1);
+  it("Deve editar o nome de um curso com sucesso", () => {
+    const novoNome = faker.lorem.words(1);
+    CreateCoursePage.name.clear().type(novoNome);
+    CreateCoursePage.submitButton.click();
+    cy.wait(5000);
+    cy.visit("/curso/edit/127");
+    cy.get("#name").should("have.value", novoNome);
+  });
 
-    EditCoursePage.visit();
-    EditCoursePage.visitPageIndexNine();
-    cy.get('[data-curso-id="127"]').click();
-    CreateCoursePage.name.clear();
-    CreateCoursePage.name.type(courseNameMock);
+  it("Deve recusar edição de nome com menos de dois caracteres", () => {
+    const nomeCurto = faker.lorem.word(1);
+    CreateCoursePage.name.clear().type(nomeCurto);
     CreateCoursePage.submitButton.click();
     cy.get("#erro-nome").should("be.visible");
   });
 
-  it("Deve editar o campo de descrição de um curso com sucesso", () => {
-    let courseDescriptionMock = faker.lorem.paragraph();
-    EditCoursePage.visit();
-    EditCoursePage.visitPageIndexNine();
-    cy.get('[data-curso-id="127"]').click();
-    CreateCoursePage.descricao.clear();
-    CreateCoursePage.descricao.type(courseDescriptionMock);
+  it("Deve editar a descrição de um curso com sucesso", () => {
+    const novaDescricao = faker.lorem.paragraph();
+    CreateCoursePage.descricao.clear().type(novaDescricao);
     CreateCoursePage.submitButton.click();
     cy.wait(5000);
-    cy.visit("curso/edit/127");
-    cy.get("#descricao").should("have.value", courseDescriptionMock);
+    cy.visit("/curso/edit/127");
+    cy.get("#descricao").should("have.value", novaDescricao);
   });
 
-  it("Deve editar o campo de objetivos de aprendizagem de um curso com sucesso", () => {
-    let courseObjectivesMock = faker.lorem.sentences(2);
-    EditCoursePage.visit();
-    EditCoursePage.visitPageIndexNine();
-    cy.get('[data-curso-id="127"]').click();
-    CreateCoursePage.objetivosAprendizagem.clear();
-    CreateCoursePage.objetivosAprendizagem.type(courseObjectivesMock);
+  it("Deve editar os objetivos de aprendizagem com sucesso", () => {
+    const novosObjetivos = faker.lorem.sentences(2);
+    CreateCoursePage.objetivosAprendizagem.clear().type(novosObjetivos);
     CreateCoursePage.submitButton.click();
     cy.wait(5000);
-    cy.visit("curso/edit/127");
-    cy.get("#objetivos_aprendizagem").should(
-      "have.value",
-      courseObjectivesMock
-    );
+    cy.visit("/curso/edit/127");
+    cy.get("#objetivos_aprendizagem").should("have.value", novosObjetivos);
   });
 
-  it("Deve editar o campo de carga horaria de um curso com sucesso", () => {
-    let courseTimeMock = faker.number.int({ min: 1, max: 60 }).toString();
-    EditCoursePage.visit();
-    EditCoursePage.visitPageIndexNine();
-    cy.get('[data-curso-id="127"]').click();
-    CreateCoursePage.cargaHoraria.clear();
-    CreateCoursePage.cargaHoraria.type(courseTimeMock);
+  it("Deve editar a carga horária de um curso com sucesso", () => {
+    const novaCarga = faker.number.int({ min: 1, max: 60 }).toString();
+    CreateCoursePage.cargaHoraria.clear().type(novaCarga);
     CreateCoursePage.submitButton.click();
     cy.wait(5000);
-    cy.visit("curso/edit/127");
-    cy.get("#carga_horaria").should("have.value", courseTimeMock);
+    cy.visit("/curso/edit/127");
+    cy.get("#carga_horaria").should("have.value", novaCarga);
   });
 });
